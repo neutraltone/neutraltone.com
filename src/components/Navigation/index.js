e4fe01bfc15f481d6946c23aa4d507beb53a55db
Tony Phipps <tonyphipps@gmail.com>
2019-01-10 12:22:54 +0000

    :speech_balloon: update copy

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -8,11 +8,13 @@ const Navigation = props => {
     <div className="fixed pin-x pin-b bg-black">
       <nav className="flex justify-between max-w-xl mx-auto p-4 md:py-8 text-sm">
         <ul className="flex items-center -mx-4 list-reset">
-          <li className="px-4">
-            <Link to="/articles" className="no-underline text-white">
-              Articles
+          {/*
+            <li className="px-4">
+              <Link to="/articles" className="no-underline text-white">
+                Articles
             </Link>
           </li>
+          */}
           <li>
             <Link to="/contact" className="no-underline text-white">
               Contact
